ce704a964ecf145d751db0e4d60df41a82081754
Leonardo Rocha <125581083+Leonardor0chaa@users.noreply.github.com>
2026-01-28 22:10:03 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -351,7 +351,7 @@ function exibirTextoNaTela(tag, texto) {
      if ('speechSynthesis' in window) {
         let utterance = new SpeechSynthesisUtterance(texto);
         utterance.lang = 'pt-BR'; 
-        utterance.rate = 1.7; 
+        utterance.rate = 1.2; 
         window.speechSynthesis.speak(utterance); 
     } else {
         console.log("Web Speech API não suportada neste navegador.");
